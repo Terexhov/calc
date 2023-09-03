8bab39b2abcdb070e8263f23d65cfd33c4921696
Denis <43913072+Terexhov@users.noreply.github.com>
2023-09-03 21:27:29 +0300

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,72 @@
+import java.util.TreeMap;
 
+public class Converter {
+    TreeMap<Character, Integer> rimKeyMap = new TreeMap<>();
+    TreeMap<Integer, String> arabKeyMap = new TreeMap<>();
+
+    public Converter() {
+        rimKeyMap.put('0', 0);
+        rimKeyMap.put('I', 1);
+        rimKeyMap.put('V', 5);
+        rimKeyMap.put('X', 10);
+        rimKeyMap.put('L', 50);
+        rimKeyMap.put('C', 100);
+        rimKeyMap.put('D', 500);
+
+
+        arabKeyMap.put(1000, "M");
+        arabKeyMap.put(900, "CM");
+        arabKeyMap.put(500, "D");
+        arabKeyMap.put(400, "CD");
+        arabKeyMap.put(100, "C");
+        arabKeyMap.put(90, "XC");
+        arabKeyMap.put(50, "L");
+        arabKeyMap.put(40, "XL");
+        arabKeyMap.put(10, "X");
+        arabKeyMap.put(9, "IX");
+        arabKeyMap.put(5, "V");
+        arabKeyMap.put(4, "IV");
+        arabKeyMap.put(1, "I");
+
+
+    }
+
+
+    public boolean isRim(String number){
+        return rimKeyMap.containsKey(number.charAt(0));
+    }
+    public String intToRim(int number) {
+        if (number <= 0) {
+            return "В римской системе нет нуля и отрицательных чисел. Попробуйте еще раз!";
+        }
+
+        String rim = "";
+        int arabKey;
+        do {
+            arabKey = arabKeyMap.floorKey(number);
+            rim += arabKeyMap.get(arabKey);
+            number -= arabKey;
+        } while (number != 0);
+
+        return rim;
+    }
+    public int rimToInt(String s) {
+        int end = s.length() - 1;
+        char[] arr = s.toCharArray();
+        int arab;
+        int result = rimKeyMap.get(arr[end]);
+        for (int i = end - 1; i >= 0; i--) {
+            arab = rimKeyMap.get(arr[i]);
+
+            if (arab < rimKeyMap.get(arr[i + 1])) {
+                result -= arab;
+            } else {
+                result += arab;
+            }
+
+
+        }
+        return result;
+
+    }
+}
